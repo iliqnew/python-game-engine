905a09a4b38b2d6bbb18098cc7e3178214f15475
i.iliev <i.t.iliev@armf.bg>
2023-12-04 01:32:16 +0200

    updating 2d position component

--- components.py
+++ components.py
@@ -1,5 +1,5 @@
 from base.component import Component
-
+import numpy as np
 
 class Render2DComponent(Component):
     def __init__(self, sprite):
@@ -8,6 +8,22 @@ class Render2DComponent(Component):
 
 class Position2DComponent(Component):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.pos = np.array([x, y])
+        self.__velocity = np.zeros(2)
+
+    def set_x(self, x: float):
+        self.pos[0] = x - self.pos[0]
+
+    def set_y(self, y: float):
+        self.pos[1] = y - self.pos[1]
+    
+    def set_pos(self, pos: np.ndarray):
+        self.pos = pos
+
+    def set_velocity(self, velocity: np.ndarray):
+        self.__velocity = velocity
+
+    def update(self):
+        self.pos += self.__velocity
+        self.__velocity = np.zeros(2)
 
